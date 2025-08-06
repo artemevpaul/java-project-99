eda37506a3e8f1ffcfeb05bca25bbccc7b553f52
Pavel Artemev <damention@MacBook-Pro.local>
2025-08-06 14:51:13 +0200

    spring init + dockerfile

---
+++ app/src/main/java/hexlet/code/app/controller/api/WelcomeController.java
@@ -0,0 +1,12 @@
+package hexlet.code.app.controller.api;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class WelcomeController {
+    @GetMapping(path = "")
+    public String welcome() {
+        return "Welcome to the App!";
+    }
+}
