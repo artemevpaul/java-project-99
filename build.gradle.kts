3346d314fb5825269a0321a31ed916ad241621c8
Pavel Artemev <damention@MacBook-Pro.local>
2025-08-08 12:04:44 +0200

    step 1, add User model, applicayion.yml

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,10 @@
 plugins {
 	java
 	application
-	checkstyle
+	//checkstyle
 	id("org.springframework.boot") version "3.5.4"
 	id("io.spring.dependency-management") version "1.1.7"
+	alias(libs.plugins.lombok)
 
 }
 
@@ -23,10 +24,34 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
-	implementation("org.springframework.boot:spring-boot-starter-web")
+	///Spring
+	implementation(libs.springBootStarterWeb)
+	implementation(libs.springBootStarterDataJpa)
+	implementation(libs.springBootStarterValidation)
+	implementation(libs.springBootStarterActuator)
+	implementation(libs.springBootStarterSecurity)
+	implementation(libs.springBootStarterOauth2ResourceServer)
+	implementation(libs.springBootDevtools)
+	implementation(libs.springBootConfigProcessor)
+
+	// Utilities
+	implementation(libs.jacksonDatabindNullable)
+	implementation(libs.commonsLang3)
+	implementation(libs.datafaker)
+	implementation(libs.instancioJunit)
+	implementation(libs.jsonunitAssertj)
+
+	// MapStruct
+	implementation(libs.mapstruct)
+	annotationProcessor(libs.mapstructProcessor)
+
+	//Test
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+	//DB
+	runtimeOnly(libs.h2)
+
 }
 
 tasks.withType<Test> {
---
+++ src/main/java/hexlet/code/model/User.java
@@ -0,0 +1,99 @@
+package hexlet.code.model;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@ToString(includeFieldNames = true, onlyExplicitlyIncluded = true)
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Table(name = "users")
+public class User implements UserDetails, BaseEntity {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @ToString.Include
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    // EMAIL
+    @Column(unique = true)
+    @Email
+    @ToString.Include
+    private String email;
+
+    // @NotBlank
+    @ToString.Include
+    private String firstName;
+
+    // @NotBlank
+    @ToString.Include
+    private String lastName;
+
+    private String passwordDigest;
+
+    @LastModifiedDate
+    private LocalDate updatedAt;
+
+    @CreatedDate
+    private LocalDate createdAt;
+
+    @Override
+    public String getPassword() {
+        return passwordDigest;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return new ArrayList<GrantedAuthority>();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+}
